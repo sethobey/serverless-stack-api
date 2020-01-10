06e39fe9e47c9c55bf43ceb943c4049b053f5d0f
Seth Obey <sethobey@gmail.com>
2020-01-10 01:55:57 -0600

    create list.js

---
+++ list.js
@@ -0,0 +1,23 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+    const params = {
+        TableName: process.env.tableName,
+        // 'Key' defines the partition key and sort key of the item to be retrieved
+        // - 'userId': Identity Pool identity id of the authenticated user
+        // - 'noteId': path parameter
+        KeyConditionalExpression: "userId = :userId",
+        ExpressionAttributeValues: {
+            ":userId": event.requestContext.identity.cognitoIdentityId
+        }
+    }
+    
+    try {
+        const result = await dynamoDbLib.call("query", params);
+        // Return the matching list of items in response body
+        return success(result.Items);
+    } catch (e) {
+        return failure({ status: false });
+    }
+};
